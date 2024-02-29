b44ab9ca3acf66a8f44f6c02cdf3a568522e3dc7
Stilan <40999153+Stilan@users.noreply.github.com>
2024-02-29 20:01:02 +0700

    Миграции Добавить в проект flyway или liquibase, через которые накатываются sql скрипты всех сущностей

--- src/main/java/com/example/mywarehouse/dto/EntranceProductDto.java
+++ src/main/java/com/example/mywarehouse/dto/EntranceProductDto.java
@@ -2,10 +2,7 @@ package com.example.mywarehouse.dto;
 
 import lombok.Data;
 import java.util.UUID;
-//        Поступление (Номер, Склад, Список товаров).
-//        Заводится при поступлении товара. Содержит список товаров,
-//        их количество и закупочные цены.
-//        В документе указывается склад, на который поступают товары.
+
 @Data
 public class EntranceProductDto {
 
--- src/test/java/com/example/mywarehouse/service/StockServiceTest.java
+++ src/test/java/com/example/mywarehouse/service/StockServiceTest.java
@@ -11,8 +11,13 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 class StockServiceTest {
@@ -26,32 +31,75 @@ class StockServiceTest {
     @Mock
     private StockMapper stockMapper;
 
-
     @Test
     void createStock() {
-//        StockDto stockDto = new StockDto();
-//        stockDto.setName("111");
-
-//        Mockito.when(stockRepository.save(any(Stock.class))).thenReturn(stockMapper.toEntity(stockDto));
+        StockDto stockDto = new StockDto();
+        stockDto.setName("111");
+        Stock stock = new Stock();
+        stock.setName("111");
+        Mockito.when(stockRepository.save(any())).thenReturn(stock).getMock();
+        Mockito.when(stockMapper.toEntity(stockDto)).thenReturn(stock).getMock();
+        Mockito.when(stockMapper.toDto(stock)).thenReturn(stockDto);
+        StockDto dto = stockService.createStock(stockDto);
+        assertThat(dto).isNotNull();
     }
 
     @Test
     void getStockDtoById() {
+        Stock stock = new Stock();
+        stock.setId(UUID.fromString("db9d6441-c3f2-4d8d-83fb-c07298610c34"));
+        stock.setName("111");
+        Mockito.when(stockRepository.findByIdAndIsDeletedFalse(UUID.fromString("db9d6441-c3f2-4d8d-83fb-c07298610c34")))
+                .thenReturn(Optional.of(stock));
+        StockDto dto = stockService.getStockDtoById(UUID.fromString("db9d6441-c3f2-4d8d-83fb-c07298610c34"));
+        assertEquals("111", stock.getName());
     }
 
     @Test
     void updateStock() {
+        StockDto stockDto = new StockDto();
+        stockDto.setName("111");
+        Stock stock = new Stock();
+        stock.setName("111");
+        Mockito.when(stockMapper.toEntity(stockDto)).thenReturn(stock).getMock();
+        stockDto.setName("222");
+        Mockito.when(stockRepository.save(any())).thenReturn(stock).getMock();
+        Mockito.when(stockMapper.toDto(stock)).thenReturn(stockDto);
+        StockDto dto = stockService.createStock(stockDto);
+        assertEquals("222", dto.getName());
     }
 
     @Test
     void deleteStock() {
+        Stock stock = new Stock();
+        stock.setId(UUID.fromString("db9d6441-c3f2-4d8d-83fb-c07298610c34"));
+        stock.setName("111");
+        Mockito.when(stockRepository.getReferenceById(UUID.fromString("db9d6441-c3f2-4d8d-83fb-c07298610c34")))
+                .thenReturn(stock);
+        StockDto stockDto = new StockDto();
+        Mockito.when(stockMapper.toDto(stock)).thenReturn(stockDto);
+        stockDto = stockService.deleteStock(UUID.fromString("db9d6441-c3f2-4d8d-83fb-c07298610c34"));
+        assertNull(stockDto.getName());
+
     }
 
     @Test
     void getStockOfName() {
+        Stock stock = new Stock();
+        stock.setName("111");
+        Mockito.when(stockRepository.findByName("111")).thenReturn(stock);
+        Stock result = stockService.getStockOfName("111");
+        assertEquals("111", result.getName());
     }
 
     @Test
     void getStockById() {
+        Stock stock = new Stock();
+        stock.setId(UUID.fromString("db9d6441-c3f2-4d8d-83fb-c07298610c34"));
+        stock.setName("111");
+        Mockito.when(stockRepository.findByIdAndIsDeletedFalse(UUID.fromString("db9d6441-c3f2-4d8d-83fb-c07298610c34")))
+                .thenReturn(Optional.of(stock));
+        Stock result = stockService.getStockById(UUID.fromString("db9d6441-c3f2-4d8d-83fb-c07298610c34"));
+        assertEquals("111", result.getName());
     }
 }
