f259f101d081b5cd906aa3e7160ad0e96cf2fded
Stilan <40999153+Stilan@users.noreply.github.com>
2024-03-07 15:16:02 +0700

    Task-6: Создать экспорт HTML В существующую реализацию HtmlFileService добавить реализацию метода ResponseEntity<Resource> getFile(List<Product> products, String name)

--- src/main/java/com/example/mywarehouse/service/file/HtmlFileService.java
+++ src/main/java/com/example/mywarehouse/service/file/HtmlFileService.java
@@ -2,7 +2,9 @@ package com.example.mywarehouse.service.file;
 
 import com.example.mywarehouse.entity.Product;
 import com.example.mywarehouse.enums.FileType;
+import com.example.mywarehouse.utility.FileName;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -10,6 +12,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Service
@@ -17,13 +23,37 @@ import java.util.List;
 @Transactional
 public class HtmlFileService implements FileService {
 
+    private static final String ARTICLE = "Артикул";
+    private static final String NAME = "Наименование";
+    private static final String REMAINDER = "Остаток";
     private static final String SAVE_AS = "attachment; filename=";
+    private static final String EXCEPTION = "fail to import data to HTML file: ";
     private static final String FILE_TYPE = ".html";
 
+
+    public static ByteArrayInputStream getByteArrayInputStream(List<Product> products) {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try (PrintWriter printWriter = new PrintWriter(out,true, StandardCharsets.UTF_8)) {
+            for (Product product : products) {
+                printWriter.write("<html>\n" + "<header></header>\n"
+                        + "<body>\n" + "<table>\n"
+                        + "<tr>" + "<th>" + ARTICLE + "</th>" + "<th>" + NAME + "</th>" + "<th>" + REMAINDER + "</th>" + "</tr>"
+                        + "<tr>" + "<td>" + product.getArticle() + "</td>" + "<td>" + product.getName() + "</td>" + "<td>" + product.getRemainingGoods() + "</td>" + "</tr>\n"
+                        + "<table>\n" + "</body>\n" + "</html>");
+            }
+        } catch (RuntimeException e) {
+            throw new RuntimeException(EXCEPTION + e.getMessage());
+        }
+        return new ByteArrayInputStream(out.toByteArray());
+    }
     @Override
     public ResponseEntity<Resource> getFile(List<Product> products, String name) {
-        return null;
-
+        InputStreamResource inputStreamResource = new InputStreamResource(HtmlFileService.getByteArrayInputStream(products));
+        String str = FileName.getFileName(name);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, SAVE_AS + str + FILE_TYPE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(inputStreamResource);
     }
 
     @Override
